1ec02bcbc0d186c51542fa8b3af5a9c731a24a25
YCWong <wangyichen0007@163.com>
2024-05-30 14:06:43 +0800

    update: dev

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,9 +15,9 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    'no-unused-vars': 'off',
+    'react-refresh/only-export-components': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
-    'react-refresh/only-export-components': 'off',
   },
 }
